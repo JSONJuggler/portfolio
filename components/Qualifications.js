f3cdae61bc84347d77ed2bde0a9782f4d556efcd
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-08 06:09:36 -0500

    using font awesome via react components, might need to bundle font awesome css with nextjs if cons are big on pages first load, no sign up etc

--- components/Qualifications.js
+++ components/Qualifications.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
 
 const Qualifications = () => {
   return (
@@ -7,6 +9,7 @@ const Qualifications = () => {
       <div>Javascript, HTML5, CSS, C#</div>
       <div>Frameworks and Technologies:</div>
       <div>React, NodeJS, CSS, C#</div>
+      <FontAwesomeIcon icon={faJs} size="2x" />
     </Fragment>
   );
 };
