9cfa86196ee3ae11dedf1e0d27959bb7458cc53b
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-09 02:37:19 -0500

    icon organization

--- components/Qualifications.js
+++ components/Qualifications.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import Hidden from "@material-ui/core/Hidden";
 import {
   SvgJavascript,
@@ -17,7 +18,7 @@ import {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    background: "black"
+    background: "#FFFFFF"
   },
   entry: {
     display: "flex",
@@ -32,152 +33,218 @@ const Qualifications = () => {
   const classes = useStyles();
 
   return (
-    <Fragment>
+    <div className={classes.root}>
       <Hidden xsDown>
-        <Grid container spacing={2} justify="center" alignItems="center">
-          <Grid item xs={12}>
-            <Typography align="center">Tools of Choice</Typography>
+        <Container maxWidth="sm">
+          <Grid container spacing={2} justify="center" alignItems="center">
+            <Grid item xs={12}>
+              <Typography variant="h4" align="center">
+                Tools of Choice
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  Javascript
+                </Typography>
+                <SvgJavascript className={classes.icon} size="5x" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  HTML5
+                </Typography>
+                <SvgHtml5 className={classes.icon} size="5x" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  CSS
+                </Typography>
+                <SvgCss className={classes.icon} size="5x" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  NodeJS
+                </Typography>
+                <SvgNodeJs className={classes.icon} size="5x" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  NextJS
+                </Typography>
+                <SvgNextjs
+                  className={classes.icon}
+                  height="50px"
+                  width="80px"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  React
+                </Typography>
+                <SvgReact className={classes.icon} size="5x" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  Redux
+                </Typography>
+                <SvgRedux className={classes.icon} height="55px" width="55px" />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  MongoDB
+                </Typography>
+                <SvgMongodb
+                  className={classes.icon}
+                  height="40px"
+                  width="80px"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div className={classes.entry}>
+                <Typography variant="subtitle1" align="center">
+                  Material-UI
+                </Typography>
+                <SvgMaterialui
+                  className={classes.icon}
+                  height="55px"
+                  width="55px"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5" align="center">
+                Other Technologies
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                FireBase
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                PostgreSQL
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                Command Line
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                C#
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                Unity
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                Socket.IO
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="center">
+                Rollbar
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">Javascript</Typography>
-              <SvgJavascript className={classes.icon} size="5x" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">HTML5</Typography>
-              <SvgHtml5 className={classes.icon} size="5x" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">CSS</Typography>
-              <SvgCss className={classes.icon} size="5x" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">NodeJS</Typography>
-              <SvgNodeJs className={classes.icon} size="5x" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">NextJS</Typography>
-              <SvgNextjs className={classes.icon} height="50px" width="80px" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">React</Typography>
-              <SvgReact className={classes.icon} size="5x" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">Redux</Typography>
-              <SvgRedux className={classes.icon} height="55px" width="55px" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">MongoDB</Typography>
-              <SvgMongodb className={classes.icon} height="40px" width="80px" />
-            </div>
-          </Grid>
-          <Grid item>
-            <div className={classes.entry}>
-              <Typography align="center">Material-UI</Typography>
-              <SvgMaterialui
-                className={classes.icon}
-                height="55px"
-                width="55px"
-              />
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography align="center">Other Technologies</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">FireBase</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">PostgreSQL</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">Command Line</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">C#</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">Unity</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">Socket.IO</Typography>
-          </Grid>
-          <Grid item>
-            <Typography align="center">Rollbar</Typography>
-          </Grid>
-        </Grid>
+        </Container>
       </Hidden>
       <Hidden smUp>
         <Grid container spacing={2} justify="center" alignItems="center">
           <Grid item xs={12}>
-            <Typography align="center">Tools of Choice</Typography>
+            <Typography variant="h4" align="center">
+              Tools of Choice
+            </Typography>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">Javascript</Typography>
+              <Typography variant="subtitle1" align="center">
+                Javascript
+              </Typography>
               <SvgJavascript className={classes.icon} size="2x" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">HTML5</Typography>
+              <Typography variant="subtitle1" align="center">
+                HTML5
+              </Typography>
               <SvgHtml5 className={classes.icon} size="2x" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">CSS</Typography>
+              <Typography variant="subtitle1" align="center">
+                CSS
+              </Typography>
               <SvgCss className={classes.icon} size="2x" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">NodeJS</Typography>
+              <Typography variant="subtitle1" align="center">
+                NodeJS
+              </Typography>
               <SvgNodeJs className={classes.icon} size="2x" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">NextJS</Typography>
+              <Typography variant="subtitle1" align="center">
+                NextJS
+              </Typography>
               <SvgNextjs className={classes.icon} height="25px" width="40px" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">React</Typography>
+              <Typography variant="subtitle1" align="center">
+                React
+              </Typography>
               <SvgReact className={classes.icon} size="2x" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">Redux</Typography>
+              <Typography variant="subtitle1" align="center">
+                Redux
+              </Typography>
               <SvgRedux className={classes.icon} height="23px" width="23px" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">MongoDB</Typography>
+              <Typography variant="subtitle1" align="center">
+                MongoDB
+              </Typography>
               <SvgMongodb className={classes.icon} height="20px" width="60px" />
             </div>
           </Grid>
-          <Grid item>
+          <Grid item xs={4}>
             <div className={classes.entry}>
-              <Typography align="center">Material-UI</Typography>
+              <Typography variant="subtitle1" align="center">
+                Material-UI
+              </Typography>
               <SvgMaterialui
                 className={classes.icon}
                 height="23px"
@@ -186,32 +253,48 @@ const Qualifications = () => {
             </div>
           </Grid>
           <Grid item xs={12}>
-            <Typography align="center">Other Technologies</Typography>
+            <Typography variant="h5" align="center">
+              Other Technologies
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">FireBase</Typography>
+            <Typography variant="body2" align="center">
+              FireBase
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">PostgreSQL</Typography>
+            <Typography variant="body2" align="center">
+              PostgreSQL
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">Command Line</Typography>
+            <Typography variant="body2" align="center">
+              Command Line
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">C#</Typography>
+            <Typography variant="body2" align="center">
+              C#
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">Unity</Typography>
+            <Typography variant="body2" align="center">
+              Unity
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">Socket.IO</Typography>
+            <Typography variant="body2" align="center">
+              Socket.IO
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography align="center">Rollbar</Typography>
+            <Typography variant="body2" align="center">
+              Rollbar
+            </Typography>
           </Grid>
         </Grid>
       </Hidden>
-    </Fragment>
+    </div>
   );
 };
 
