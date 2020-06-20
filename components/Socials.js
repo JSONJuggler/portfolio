ce1ceb8e54959d147cb6f25b53efbc99523728ec
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-20 16:04:49 -0500

    socials section

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,27 @@
+import { SvgYoutube, SvgTwitter, SvgGithub } from "../icons/icons";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <Grid container spacing={4}>
+      <Grid item>
+        <SvgGithub size="2x" />
+      </Grid>
+      <Grid item>
+        <SvgYoutube size="2x" />
+      </Grid>
+      <Grid item>
+        <SvgTwitter size="2x" />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Socials;
