b1b50a18b62c3022204a0feae05a72cb547e0fa3
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-21 18:35:21 -0500

    height based on viewport with an exception for ios mobiles, those heights will be smaller to account for the navigation bars hiding the bouncing arrow

--- components/Landing.js
+++ components/Landing.js
@@ -6,6 +6,7 @@ import { useEffect, useCallback } from "react";
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "95vh",
+    minHeight: "calc(var(--vh, 1vh)*95)",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -32,15 +33,30 @@ const useStyles = makeStyles((theme) => ({
 const Landing = () => {
   const classes = useStyles();
 
-  const handleScroll = () => {
-    console.log("about to log");
-  };
+  //const handleResize = () => {
+  //let vh = window.innerHeight * 0.01;
+  //document.documentElement.style.setProperty("--vh", `${vh}px`);
+  //};
+
+  //useEffect(() => {
+  //window.addEventListener("resize", throttle(handleResize, 800));
+  //return () => {
+  //window.removeEventListener("resize", handleResize);
+  //};
+  //}, []);
 
   useEffect(() => {
-    window.addEventListener("scroll", throttle(handleScroll, 800));
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    //if (typeof window !== "undefined") {
+    if (
+      navigator.userAgent.indexOf("Safari") != -1 &&
+      navigator.userAgent.indexOf("Chrome") == -1 &&
+      /iPad|iPhone|iPod/.test(navigator.userAgent) &&
+      !window.MSStream
+    ) {
+      let vh = (window.innerHeight - 10) * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      //}
+    }
   }, []);
 
   return (
