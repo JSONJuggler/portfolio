89007fe6d8781a1a8f1631dd425c108c3426fb17
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-25 02:24:00 -0500

    about page

--- pages/about.js
+++ pages/about.js
@@ -18,6 +18,10 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(5),
     paddingBottom: theme.spacing(2),
   },
+  about: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(2),
+  },
   bodyText: {
     width: "100%",
     marginTop: "auto",
@@ -25,7 +29,12 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: theme.spacing(1),
   },
   gridEntry: {
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(3),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+  },
+  gridContainer: {
+    marginBottom: theme.spacing(3),
   },
 }));
 
@@ -36,51 +45,139 @@ const about = () => {
     <Container maxWidth="lg">
       <div className={classes.root}>
         <div className={classes.bodyText}>
-          <Typography variant="h4">Hello again, I'm Beau</Typography>
-          <Grid container justify="space-between">
+          <div className={classes.about}>
+            <Typography variant="h4">Hello again, I'm Beau</Typography>
+          </div>
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={4} md={3}>
+              <Typography variant="body1">
+                <b>SKILLS</b>
+              </Typography>
+            </Grid>
+            <Grid container item xs={8} md={9}>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">
+                  Responsive and performant development across devices
+                </Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Rapid prototyping</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">
+                  Data consuption, manipulation, and visualization
+                </Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Critical thinking</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Problem solving</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Time management</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Remote work</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Teamwork</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Customer service</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.gridContainer}>
             <Grid item xs={4} md={3}>
               <Typography variant="body1">
                 <b>FRONT-END</b>
               </Typography>
             </Grid>
-            <Grid container item xs={8} md={9} justify="space-between">
+            <Grid container item xs={8} md={9}>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Javascript</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">HTML5</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">CSS</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">React</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Redux</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Material-UI</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">NextJS</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={4} md={3}>
+              <Typography variant="body1">
+                <b>BACK-END</b>
+              </Typography>
+            </Grid>
+            <Grid container item xs={8} md={9}>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">NodeJS</Typography>
+              </Grid>
               <Grid item className={classes.gridEntry} xs={6} md={4}>
                 <Typography variant="body1">Express</Typography>
               </Grid>
               <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body1">Heroku</Typography>
+                <Typography variant="body1">MongoDB</Typography>
               </Grid>
               <Grid item className={classes.gridEntry} xs={6} md={4}>
                 <Typography variant="body1">FireBase</Typography>
               </Grid>
               <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">PostgreSQL</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">Command Line</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">VIM</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">Socket.IO</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">Rollbar</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">C#</Typography>
-              </Grid>
-              <Grid item className={classes.gridEntry} xs={6} md={4}>
-                <Typography variant="body2">Unity</Typography>
+                <Typography variant="body1">PostgreSQL</Typography>
               </Grid>
             </Grid>
           </Grid>
-          <Typography variant="h4">Skills</Typography>
-          <Typography variant="h4">Tools</Typography>
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={4} md={3}>
+              <Typography variant="body1">
+                <b>OTHER TOOLS</b>
+              </Typography>
+            </Grid>
+            <Grid container item xs={8} md={9}>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Git</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Heroku</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">VIM</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Command Line</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Socket.IO</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Rollbar</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">Unity</Typography>
+              </Grid>
+              <Grid item className={classes.gridEntry} xs={6} md={4}>
+                <Typography variant="body1">C#</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
-        <ContactMe />
       </div>
+      <ContactMe />
     </Container>
   );
 };
