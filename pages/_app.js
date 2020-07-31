418cb25a92e3fe27d1604bdffbea4754dad05f56
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-30 19:36:43 -0500

    og meta tag description update

--- pages/_app.js
+++ pages/_app.js
@@ -48,7 +48,7 @@ function MyApp({ Component, pageProps }) {
         <meta
           prefix="og: http://ogp.me/ns#"
           property="og:description"
-          content="Hi, I'm Beau Reescano, a full stack web developer"
+          content="Hi, I'm a full stack web developer. I create websites and webapps using cutting edge web technologies."
         />
         <meta
           prefix="og: http://ogp.me/ns#"
