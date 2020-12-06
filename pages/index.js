ff3b6a2eb8435d6722c36e64c735ced7bc387e6c
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2020-12-05 23:57:38 -0600

    Adding resume to home page

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import { Fragment } from 'react';
 
 import { makeStyles } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Landing from '../components/Landing';
 import Qualifications from '../components/Qualifications';
 import Projects from '../components/Projects';
 import ContactMe from '../components/ContactMe';
+import CustomLink from '../components/CustomLink';
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
@@ -36,6 +39,15 @@ const Home = ({ data }) => {
       <Qualifications />
       <Container maxWidth="lg">
         <Projects projects={data.projects} />
+        <Grid container spacing={2} justify="space-around" alignItems="center">
+          <Grid item xs={true} className={classes.titleDesktop}>
+            <CustomLink href="/resume" title="To Resume">
+              <Typography className={classes.underline} variant="h4" align="center">
+                Checkout my resume here!
+              </Typography>
+            </CustomLink>
+          </Grid>
+        </Grid>
         <ContactMe />
       </Container>
     </div>
